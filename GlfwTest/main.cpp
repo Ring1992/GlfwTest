2ae41fa736975017f53c8d11791e46e249ef87bc
1 <492502377@qq.com>
2020-05-27 11:10:04 +0800

    Initial Commit

---
+++ GlfwTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GlfwTest
+//
+//  Created by 1 on 2020/5/27.
+//  Copyright © 2020 Ring. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
